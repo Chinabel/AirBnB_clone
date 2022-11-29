6e120e5bb56e64baa549ab0a0dc5f8ab94eff795
Debo <tijanidebo@gmail.com>
2022-11-29 14:56:25 +0100

    trail without new line

--- models/state.py
+++ models/state.py
@@ -1,13 +1,8 @@
-#!/usr/bin/python3
-"""
-Defines the State class
-"""
+#!/usr/bin/python
+""" holds class State"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state
-    Attributes:
-        name (str): The name of the state
-    """
+    """Representation of state"""
     name = ""

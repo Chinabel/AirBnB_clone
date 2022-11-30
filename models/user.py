27cb21eeb2c707b7ce29671477395a98ce150457
Chineze Ogugua <christabelogugua@gmail.com>
2022-11-30 02:21:32 -0800

    fix user model

--- models/user.py
+++ models/user.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-""" Class User """
+"""
+Defines class User
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Representation of a user """
+    """Representation of a user
+
+
+    Attributes:
+        email(str): user email
+        password(str): user password
+        first_name(str): first name
+        last_name(str): last name
+
+    """
     email = ""
     password = ""
     first_name = ""
